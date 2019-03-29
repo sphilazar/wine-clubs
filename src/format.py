fa154f7eabb18f8b6022caa136d1422ad7bb2a49
sphilazar <phil.salazar64@gmail.com>
2019-03-29 08:51:26 -0700

    Pickle,GradientBoost,Partial Dependence plots

--- src/format.py
+++ src/format.py
@@ -17,7 +17,7 @@ def clean_data():
     clubs = clubs[nans + keep]
 
     # Create features
-    clubs["ltv"] = [float( "".join((ltv[1:].split(","))) ) for ltv in clubs["Lifetime Value"]]
+    clubs["ltv"] = [float("".join((ltv[1:].split(","))) ) for ltv in clubs["Lifetime Value"]]
     clubs.loc[clubs["Club Status"].isna() & clubs["Cancel Date"].isna(),"Club Status"] = "Active"
     clubs.loc[clubs["Club Status"].isna() & ~clubs["Cancel Reason"].isna(),"Club Status"] = "Cancelled"
     clubs.loc[ (clubs["Bill Birth Date"].isna() & ~clubs["Ship Birth Date"].isna()) , "Bill Birth Date"] = clubs["Ship Birth Date"]
@@ -29,13 +29,24 @@ def clean_data():
     clubs.loc[clubs["Ship State Code"].isna(),"Ship State Code"] = "CA"
     clubs.loc[clubs["Ship Zip"].isna(),"Ship Zip"] = "94515"
     clubs["isPickup"] = ~clubs["Pickup Location"].isna()
+
+    # Convert dates to time periods (float in years)
+
     clubs["cancel"] = np.array([str(cancel).split("/")[0::2] for cancel in clubs["Cancel Date"]])
     clubs["signup"] = [list(str(signup).split("/")[0::2]) for signup in clubs["Signup Date"]]
     clubs["clubLength"] = [(int(end[1]) - int(start[1])) +  ((( (int(end[0])+12) - int(start[0]) ) % 12  ) / 12 ) if (len(end)==2) else (19 - int(start[1])) +  ((( (3+12) - int(start[0]) ) % 12  ) / 12 ) for end,start in zip(clubs["cancel"],clubs["signup"]) ]
 
+    clubs["last_order"] = [list(str(signup).split("/")[0::2]) for signup in clubs["Last Order Date"]]
+    clubs["last_process"] = [list(str(signup).split("/")[0::2]) for signup in clubs["Last Processed Date"]]
+
+    # If Last Order is nan, they have just signed up. Assume 0 Time since last order if new club member
+    # Not if "Time Since Last Order" is < 0,  member ordered AFTER cancellation
+    clubs["Time Since Last Order"] = [(int(end[1]) - int(start[1])) +  ((( (int(end[0])+12) - int(start[0]) ) % 12  ) / 12 ) if ((len(end)==2) & (len(start)==2)) else 0 for end,start in zip(clubs["cancel"],clubs["last_order"]) ]
+
     # Merge Customers onto Clubs
 
     clubs = clubs.merge(customers[['Customer No.','Number Of Transactions','Lifetime Value','Last Order Date','Last Order Amount']],how="left",left_on="Customer Number",right_on="Customer No.")
+    clubs["Last Order Amount"] = [float("".join(ltv[1:].split(","))) if not (ltv[0] == "(") else float("".join(ltv[2:-1].split(",")))  for ltv in clubs["Last Order Amount"]]
 
     # Aggregate totals by Customer ID
 
@@ -44,13 +55,21 @@ def clean_data():
 
     data["Total"] = data["Quantity"] * data["Price"]
     customer_orders = data.groupby("Customer Number").sum()[["Quantity","Total"]]
-    customer_orders = customer_orders[customer_orders["Total"]!=0]
 
-    clubs = clubs.merge(customer_orders,how="left",left_on="Customer Number",right_on="Customer Number") # Note  there will be NaNs left
+    customer_orders = customer_orders[customer_orders["Quantity"]!=0]
+    customer_orders["ASP"] = customer_orders["Total"] / customer_orders["Quantity"]
+
+    clubs = clubs.merge(customer_orders,how="left",left_on="Customer Number",right_on="Customer Number") # Note there will be NaNs left
+    
+    # Eliminate NaNs
+
+    clubs.loc[clubs["Quantity"].isna(),"Quantity"] = 0
+    clubs.loc[clubs["Total"].isna(),"Total"] = 0
+    clubs.loc[clubs["ASP"].isna(),"ASP"] = 0
 
     # Return final dataframe features for fitting
     '''
-    Columns before filtering
+    #Columns before filtering
 
     current_columns = ['Club Status', 'Bill Birth Date', 'Ship Birth Date', 'Ship City',
        'Ship State Code', 'Ship Zip', 'Pickup Location', 'Cancel Date',
@@ -60,19 +79,84 @@ def clean_data():
        'Lifetime Value_x', 'ltv', 'age', 'isPickup', 'cancel', 'signup',
        'clubLength', 'Customer No.', 'Number Of Transactions',
        'Lifetime Value_y', 'Last Order Date_y', 'Last Order Amount',
-       'Quantity', 'Total']
+       'Quantity', 'Total','ASP']
     '''
-    clubs = clubs[['Club Status', 'Ship City',
+    clubs = clubs[['Club Status',
        'Ship State Code', 'Ship Zip',
-       'Cancel Reason', 'Last Processed Date', 'Last Order Date_x', 'Club',
+       'Cancel Reason', 'Club',
        'Customer Number',
-       "Shipments (note that these don't necessarily mean orders)",
-       'Lifetime Value_x', 'ltv', 'age', 'isPickup',
-       'clubLength', 'Customer No.', 'Number Of Transactions',
-       'Lifetime Value_y', 'Last Order Date_y', 'Last Order Amount',
-       'Quantity', 'Total']]
+       "Shipments (note that these don't necessarily mean orders)", 'ltv', 'age', 'isPickup',
+       'clubLength', 'Number Of Transactions', 'Last Order Amount',"Time Since Last Order",'Quantity', 'Total','ASP']]
 
-    # final_columns = 
+    # Change names for clarification
+    clubs.columns = ['Club Status',
+       'State', 'Zip',
+       'Cancel Reason', 'Club Tier',
+       'Customer ID',
+       "Shipments", 'LTV', 'Age', 'isPickup',
+       'Club Length', 'Transactions', 'Last Order Amount',"Time Since Last Order",
+       'Quantity', 'Orders Total','ASP']
 
+    # Change order of columns
+    clubs = clubs[['Customer ID','Club Tier','State','Zip','Age','isPickup','Transactions','Shipments',
+       'Quantity','Orders Total','ASP','LTV',"Time Since Last Order",'Last Order Amount',
+       'Club Status','Club Length','Cancel Reason']]
+
+    # Combine multiple entries on Clubs for a given Customer ID ("Switched Club Level")
+
+    for clubid in clubs["Customer ID"].unique():
+        if clubs[clubs["Customer ID"]==clubid].shape[0] > 1:
+            summed = clubs[clubs["Customer ID"]==clubid].groupby("Customer ID").sum()[["Shipments","Club Length","Transactions"]]
+            clubs.loc[((clubs["Customer ID"]==clubid) & (clubs["Club Status"]=="Active")),"Club Length"] = summed.loc[clubid,"Club Length"]
+            clubs.loc[((clubs["Shipments"]==clubid) & (clubs["Club Status"]=="Active")),"Shipments"] = summed.loc[clubid,"Shipments"]
+            clubs.loc[((clubs["Transactions"]==clubid) & (clubs["Club Status"]=="Active")),"Transactions"] = summed.loc[clubid,"Transactions"]
+
+            clubs.drop(clubs[((clubs["Customer ID"]==clubid) & (clubs["Club Status"]=="Cancelled"))].index,axis=0,inplace=True)
+
+    # Average transaction amount
+
+    clubs["Average Transaction"] = [ x/y if x!=0 else 0 for x,y in zip(clubs["LTV"],clubs["Transactions"])]
+
+    # Create above / below average club length feature for dumb model
+
+    clubs["Above Mean Club Length"] = (clubs["Club Length"]>=clubs["Club Length"].mean()).astype(int)
+
+    # Create explicit target. 1 for churn, 0 for not
+
+    clubs["Target"] = ~((clubs["Club Status"]=="Active") | (clubs["Club Status"]=="OnHold"))
+
+    # One hot encode membership tiers
+
+    clubs["Quarter Case"] = ((clubs["Club Tier"]=="3-Bottle") | (clubs["Club Tier"]=="3-Bottle (Industry)")).astype(int)
+    clubs["Half Case"] = (clubs["Club Tier"]=="6-Bottle").astype(int)
+    clubs["Full Case"] = (clubs["Club Tier"]=="12-Bottle").astype(int)
 
     return clubs
+
+import csv
+import random
+random.seed(32)
+
+def get_test_train_set(df):
+    TEST_SPLIT_RATIO = 0.1
+    a = np.array(df.index.tolist())
+    random.shuffle(a)
+
+    split_index = int(len(a) * TEST_SPLIT_RATIO)
+    df_test = df.loc[a][:split_index]
+    df_train = df.loc[a][split_index:]
+
+    with open('../test_set.csv', mode='w') as test:
+        clubwriter = csv.writer(test, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        clubwriter.writerow(df_test.columns)
+        for row in df_test.values:
+            clubwriter.writerow(row)
+    
+    with open('../train_set.csv', mode='w') as train:
+        clubwriter = csv.writer(train, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        clubwriter.writerow(df_train.columns)
+        for row in df_train.values:
+            clubwriter.writerow(row)
+    
+
+    
---
+++ src/model.py
@@ -0,0 +1,113 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import roc_curve,roc_auc_score
+from  sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+
+class ChurnModel:
+    def __init__(self,columns):
+        self.model = None
+        self.columns = columns
+        self.labels = None
+
+    def fit_dumb_model(self,df):
+        X = df["Club Length"].values.reshape(-1,1)
+        y = df["Target"].values.astype(int)
+        # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=16)
+        model = LogisticRegression(solver='lbfgs')
+        self.model =  model
+        self.model.fit(X,y)
+
+    def fit_a_model(self,df):
+        '''
+        Logistic regression across multiple variables
+        '''
+        X = df[self.columns].values
+        y = df["Target"].values.astype(int)
+        # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=16)
+        model = LogisticRegression(penalty='l1')
+        self.model = model
+        self.model.fit(X,y)
+
+    def fit_random_forest(self,df):
+        X = df[self.columns].values
+        y = df["Target"].astype(int)
+
+        # X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.20,random_state=16)
+
+        #flip OOB score to true
+        rf = RandomForestClassifier()
+        self.model = rf
+        self.model.fit(X,y)
+
+
+    def fit_gradient_boosted_forest(self,df,n_estimators=100,learning_rate=1):
+        X = df[self.columns].values
+        y = df["Target"].astype(int)
+        self.model = GradientBoostingClassifier(n_estimators=n_estimators, max_depth=4,
+                                    learning_rate=learning_rate, loss='exponential',
+                                    random_state=64)
+        self.model.fit(X,y)
+
+    def get_predictions(self,df_test,df_train):
+        X_test = df_test[self.columns].values
+        self.labels = df_test["Target"].values.astype(int) #y_test
+        X_train = df_train[self.columns].values
+        y_train = df_train["Target"].values.astype(int)
+
+        yhat = self.model.predict(X_test)
+        probas = self.model.predict_proba(X_test)
+        score = self.model.score(X_test,self.labels)
+        cv_scores = cross_val_score(self.model,X_train,y_train,cv=5)
+        return yhat,probas,score,cv_scores
+
+    def get_roc_curve(self,probabilities,n=100):
+
+        #just in case labels are 1's and 0's
+        self.labels = self.labels.astype(bool)
+        auc = roc_auc_score(self.labels,probabilities)
+        TPRs = []
+        FPRs = []
+        Ts = []
+        for threshold in np.linspace(0,1,n):
+
+            #set threshold boolean mask
+            y_predict = probabilities > threshold
+
+            #calculate confusion matrix stuff
+            tp = (y_predict & self.labels).sum()
+            fp = (y_predict & ~self.labels).sum()
+            tn = (~y_predict & ~self.labels).sum()
+            fn = (~y_predict & self.labels).sum()
+
+            #calc tpr and fpr for ROC curve
+            tpr = tp / (tp + fn)
+            fpr = fp / (fp + tn)
+
+            #add tpr and fpr to lists
+            TPRs.append(tpr)
+            FPRs.append(fpr)
+            Ts.append(threshold)
+
+
+        plt.figure()
+        plt.scatter(FPRs,TPRs)
+        plt.plot(Ts,Ts, 'k--')
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('ROC curve for wine club churn')
+
+        return auc,np.array(TPRs), np.array(FPRs), np.array(Ts)
+
+
+import pickle
+
+def get_pickle(model):
+    pickle.dump(model, open('model.p', 'wb'))
+
